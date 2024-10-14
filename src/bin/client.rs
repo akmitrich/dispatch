da575812c09126f8652906608bfd63650adccb7f
dav1dvorobev <dav1dvorobev@vk.com>
2024-10-14 15:47:51 +0300

    add echo-server

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,1 +1,30 @@
-fn main() {}
+use futures_util::stream::SplitStream;
+use futures_util::{SinkExt, StreamExt};
+use tokio::net::TcpStream;
+use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream};
+
+async fn handler(mut reader: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>) {
+    while let Some(msg) = reader.next().await {
+        let msg = msg.expect("failed read message");
+        if msg.is_text() {
+            print!("{}", msg);
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let (ws_stream, _) = connect_async("ws://localhost:8080")
+        .await
+        .expect("failed during websocket handshaket");
+    let (mut sender, reader) = ws_stream.split();
+    tokio::spawn(handler(reader));
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).expect("failed read");
+        sender
+            .send(Message::from(input))
+            .await
+            .expect("failed send message");
+    }
+}
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,1 +1,27 @@
-fn main() {}
+use futures_util::{SinkExt, StreamExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::accept_async;
+
+async fn handler(stream: TcpStream) {
+    let ws_stream = accept_async(stream)
+        .await
+        .expect("error during websocket handshake");
+    let (mut sender, mut reader) = ws_stream.split();
+    while let Some(msg) = reader.next().await {
+        let msg = msg.expect("failed read message");
+        if msg.is_text() {
+            print!("{}", msg);
+            sender.send(msg).await.expect("failed send message");
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080")
+        .await
+        .expect("failed bind");
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handler(stream));
+    }
+}
