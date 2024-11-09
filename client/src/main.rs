8226f7ee7edfa48fcba277d4504c5d0831b188ac
dav1dvorobev <dav1dvorobev@vk.com>
2024-11-09 21:34:49 +0300

    client: simplification

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,42 +1,30 @@
-use std::io::Write;
-use futures_util::stream::SplitStream;
 use futures_util::{SinkExt, StreamExt};
-use tokio::net::TcpStream;
-use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream};
-
-type Receiver = SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>;
+use std::io::Write;
+use tokio_tungstenite::{connect_async, tungstenite::Message};
 
 #[tokio::main]
 async fn main() {
     let (ws_stream, _) = connect_async("ws://localhost:8080")
         .await
         .expect("failed connect");
-    let (mut tx, rx) = ws_stream.split();
-    tokio::spawn(handler(rx));
-    loop {
-        tx.send(Message::from(input("")))
-            .await
-            .expect("failed send");
+    let (mut tx, mut rx) = ws_stream.split();
+    tokio::spawn(async move {
+        loop {
+            tx.send(Message::from(input()))
+                .await
+                .expect("failed send");
+        }
+    });
+    while let Some(Ok(Message::Text(msg))) = rx.next().await {
+        println!("{}", msg);
     }
 }
 
-fn input(prompt: &str) -> String {
-    print!("{}", prompt);
+fn input() -> String {
     std::io::stdout().flush().expect("failed flush");
     let mut input = String::new();
     std::io::stdin()
         .read_line(&mut input)
         .expect("falied read line");
     input.trim().to_string()
-}
-
-async fn handler(mut rx: Receiver) {
-    while let Some(msg) = rx.next().await {
-        match msg {
-            Ok(Message::Text(text)) => {
-                println!("{}", text);
-            }
-            _ => return
-        }
-    }
 }
