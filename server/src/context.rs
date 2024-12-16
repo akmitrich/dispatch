bb325d3203eed69aeffae404641d54a4797c0377
dav1dvorobev <dav1dvorobev@vk.com>
2024-12-16 12:53:22 +0300

    server: changed the key storage

--- server/src/context.rs
+++ server/src/context.rs
@@ -3,6 +3,7 @@ use crate::{
     userconnection::UserConnection,
 };
 use anyhow::Result;
+use jwt_simple::prelude::HS256Key;
 use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::{collections::HashMap, env, sync::Arc};
 use tokio::sync::RwLock;
@@ -11,14 +12,14 @@ pub type ConnectionsPool = Arc<RwLock<HashMap<String, UserConnection>>>;
 
 #[derive(Clone)]
 pub struct Context {
-    secret_key: String,
+    key: HS256Key,
     pub db_pool: PgPool,
     pub channel: Channel,
     pub conn_pool: ConnectionsPool,
 }
 impl Context {
     pub async fn create() -> Result<Self> {
-        let secret_key = env::var("SECRET_KEY")?;
+        let key = HS256Key::from_bytes(env::var("SECRET_KEY")?.as_bytes());
         let database_url = env::var("DATABASE_URL")?;
         let db_pool = PgPoolOptions::new()
             .max_connections(10)
@@ -27,13 +28,13 @@ impl Context {
         let conn_pool = Arc::new(RwLock::new(HashMap::new()));
         let channel = Channel::new(&database_url, conn_pool.clone()).await?;
         Ok(Self {
-            secret_key: secret_key,
+            key: key,
             db_pool: db_pool,
             channel: channel,
             conn_pool: conn_pool,
         })
     }
-    pub fn secret_key(&self) -> &[u8] { self.secret_key.as_bytes() }
+    pub fn key(&self) -> &HS256Key { &self.key }
     pub fn send(&self, from: &str, body: String) -> Result<()> {
         self.channel.send(ChannelMessage {
             from: from.to_string(),
--- server/src/endpoints.rs
+++ server/src/endpoints.rs
@@ -5,23 +5,22 @@ use anyhow::Result;
 use futures_util::StreamExt;
 use jwt_simple::{
     claims::{Claims, NoCustomClaims},
-    prelude::{Duration, HS256Key, MACLike},
+    prelude::{Duration, MACLike},
 };
 use tokio::{io::AsyncWriteExt, net::TcpStream};
 use tokio_tungstenite::{accept_async, tungstenite::Message};
 
 pub async fn signin(context: Context, mut socket: TcpStream, body: &str) -> Result<()> {
-    let request: AuthRequest = serde_json::from_str(body)?;
+    let userdata: AuthRequest = serde_json::from_str(body)?;
     let query = sqlx::query("SELECT * FROM users WHERE (username = $1) AND (password = $2)")
-        .bind(request.username)
-        .bind(request.password)
+        .bind(userdata.username)
+        .bind(userdata.password)
         .execute(&context.db_pool)
         .await?;
     if query.rows_affected() != 0 {
-        if !context.contains(request.username).await {
-            let key = HS256Key::from_bytes(context.secret_key());
-            let claims = Claims::create(Duration::from_mins(1)).with_subject(request.username);
-            let token = key.authenticate(claims)?;
+        if !context.contains(userdata.username).await {
+            let claims = Claims::create(Duration::from_mins(1)).with_subject(userdata.username);
+            let token = context.key().authenticate(claims)?;
             socket.write(&Response::new(200, "OK", &token)).await?;
         } else {
             socket
@@ -41,16 +40,16 @@ pub async fn signin(context: Context, mut socket: TcpStream, body: &str) -> Resu
 }
 
 pub async fn signup(context: Context, mut socket: TcpStream, body: &str) -> Result<()> {
-    let request: AuthRequest = serde_json::from_str(body)?;
+    let userdata: AuthRequest = serde_json::from_str(body)?;
     let query = sqlx::query("SELECT * FROM users WHERE username = $1")
-        .bind(request.username)
+        .bind(userdata.username)
         .execute(&context.db_pool)
         .await?;
     if query.rows_affected() == 0 {
         socket.write(&Response::new(200, "OK", "Success")).await?;
         sqlx::query("INSERT INTO users (username, password) VALUES ($1, $2)")
-            .bind(request.username)
-            .bind(request.password)
+            .bind(userdata.username)
+            .bind(userdata.password)
             .execute(&context.db_pool)
             .await?;
     } else {
@@ -65,8 +64,7 @@ pub async fn connect(mut context: Context, socket: TcpStream, headers: &str) -> 
     if headers.contains("Upgrade: websocket") {
         let mut ws_stream = accept_async(socket).await?;
         if let Some(Ok(Message::Text(token))) = ws_stream.next().await {
-            let key = HS256Key::from_bytes(context.secret_key());
-            match key.verify_token::<NoCustomClaims>(&token, None) {
+            match context.key().verify_token::<NoCustomClaims>(&token, None) {
                 Ok(claims) => {
                     let username = claims.subject.unwrap();
                     let (ws_tx, mut ws_rx) = ws_stream.split();
