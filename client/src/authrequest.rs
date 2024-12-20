e6dddf65628e35afe3c1f0497eae0bc10e42f483
dav1dvorobev <dav1dvorobev@vk.com>
2024-12-20 22:35:30 +0300

    client: added ascii checker

--- client/src/authrequest.rs
+++ client/src/authrequest.rs
@@ -8,7 +8,8 @@ pub struct AuthRequest<'a> {
 impl<'a> AuthRequest<'a> {
     pub fn is_valid(&self) -> bool {
         let is_empty = self.username.is_empty() || self.password.is_empty();
-        let correct_length = self.username.len() <= 8 && self.password.len() <= 8; 
-        !is_empty && correct_length
+        let correct_length = self.username.len() <= 8 && self.password.len() <= 8;
+        let is_ascii = self.username.is_ascii() && self.password.is_ascii();
+        !is_empty && correct_length && is_ascii
     }
 }
