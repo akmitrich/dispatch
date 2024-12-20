930b4226e48f02d3de79c2d1a943ec818d02688b
dav1dvorobev <dav1dvorobev@vk.com>
2024-12-20 22:36:19 +0300

    server: added ascii checker

--- server/src/authrequest.rs
+++ server/src/authrequest.rs
@@ -9,6 +9,7 @@ impl<'a> AuthRequest<'a> {
     pub fn is_valid(&self) -> bool {
         let is_empty = self.username.is_empty() || self.password.is_empty();
         let correct_length = self.username.len() <= 8 && self.password.len() <= 8; 
-        !is_empty && correct_length
+        let is_ascii = self.username.is_ascii() && self.password.is_ascii();
+        !is_empty && correct_length && is_ascii
     }
 }
